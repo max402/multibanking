92bcae370d05119446db18e02da127521c722411
clovis.gakam@gmail.com <clovis.gakam@gmail.com>
2017-05-17 09:52:07 +0100

    remove unnecessary files

--- onlinebanking-mock/src/main/java/de/adorsys/multibanking/controller/OnlinebankingMockController.java
+++ onlinebanking-mock/src/main/java/de/adorsys/multibanking/controller/OnlinebankingMockController.java
@@ -2,8 +2,8 @@ package de.adorsys.multibanking.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,13 +17,13 @@ public class OnlinebankingMockController {
 	private OnlineBankingMockService mockService;
 
     @PostMapping("/accounts")
-    public Resource<String> createBankAccounts(AddBankAccountsRequest request) {
+    public Resource<String> createBankAccounts(@RequestBody AddBankAccountsRequest request) {
 		mockService.addBankAccounts(request.getAccounts(), request.getBankAccess());
         return new Resource<String>("ok");
     }
 
     @PostMapping("/bookings")
-    public Resource<String> getBookings(AddBookingsRequest request) {
+    public Resource<String> getBookings(@RequestBody AddBookingsRequest request) {
 		mockService.addBookings(request.getBookings(), request.getBankAccess(), request.getBankAccount());
         return new Resource<String>("ok");
     }

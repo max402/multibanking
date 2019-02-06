03e8aa07e38c5d198f0058b4f4d6f1fe603d255d
pspex <pspex@adorsys.de>
2019-02-06 16:30:06 +0100

    MUL-326 erster Test

--- onlinebanking-mapper/src/test/java/org/adorsys/multibanking/onlinebanking/mapper/api/MapperTest.java
+++ onlinebanking-mapper/src/test/java/org/adorsys/multibanking/onlinebanking/mapper/api/MapperTest.java
@@ -2,11 +2,11 @@ package org.adorsys.multibanking.onlinebanking.mapper.api;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.adorsys.cryptoutils.exceptions.BaseExceptionHandler;
 import org.adorsys.multibanking.onlinebanking.mapper.api.domain.UserData;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,19 +21,16 @@ public class MapperTest {
     public void a() {
         try {
             String content = readFile("structure.example.json");
-            LOGGER.info(" before:" + content);
+            LOGGER.debug(" before:" + content);
 
             TypeReference<UserData> rootType = new TypeReference<UserData>() {};
             ObjectMapper om = new ObjectMapper();
-//            om.enable(SerializationFeature.INDENT_OUTPUT);
-
             UserData userData = om.readValue(content, rootType);
             String reloadedContent = om.writerWithDefaultPrettyPrinter().writeValueAsString(userData);
 
-            LOGGER.info(" after: " + reloadedContent);
+            LOGGER.debug(" after: " + reloadedContent);
 
-            int compare = StringUtils.compare(content, reloadedContent);
-            LOGGER.info("compare is " + compare);
+            Assert.assertEquals(0, StringUtils.compare(content, reloadedContent));
         } catch (Exception e) {
             throw BaseExceptionHandler.handle(e);
         }

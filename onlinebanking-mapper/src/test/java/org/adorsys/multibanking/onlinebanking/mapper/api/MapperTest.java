3a0742acb21d51fbb6f78bab46afdb67cdb3eef6
pspex <pspex@adorsys.de>
2019-02-12 20:09:27 +0100

    MUL-326 print class members for mapper

--- onlinebanking-mapper/src/test/java/org/adorsys/multibanking/onlinebanking/mapper/api/MapperTest.java
+++ onlinebanking-mapper/src/test/java/org/adorsys/multibanking/onlinebanking/mapper/api/MapperTest.java
@@ -15,6 +15,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
 /**
  * Created by peter on 06.02.19 12:34.
  */
@@ -48,12 +52,11 @@ public class MapperTest {
             String user = "p.spiessbach";
             String pin = "11111";
 
-            LoadAccountInformationResponse response;
             SimpleMockBanking simpleMockBanking = new SimpleMockBanking(null, null);
             BankAccess bankAccess = new BankAccess();
             bankAccess.setBankLogin(user);
 
-            response = simpleMockBanking.loadBankAccounts(
+            LoadAccountInformationResponse response = simpleMockBanking.loadBankAccounts(
                     null,
                     LoadAccountInformationRequest.builder()
                             .bankApiUser(null)
@@ -79,6 +82,55 @@ public class MapperTest {
         }
 
     }
+
+    @Test
+    public void printMembers() {
+        printMembers("", LoadAccountInformationResponse.class);
+    }
+
+    private void printMembers(String indent, Class<?> clazz) {
+        String space = indent + " " + clazz.getName() + " ";
+        for (Field field : clazz.getDeclaredFields()) {
+            if (("" + field.getType()).contains("interface")) {
+                LOGGER.info(1 + space + field.getName() + " (" + field.getGenericType() + ")");
+                ParameterizedType parameterizedType = (ParameterizedType) field.getGenericType();
+                int numberOfTemplateParams = parameterizedType.getActualTypeArguments().length;
+                for (int param = 0; param < numberOfTemplateParams; param++) {
+                    Type type = parameterizedType.getActualTypeArguments()[param];
+                    if (type instanceof Class) {
+                        Class<?> paramClazz = (Class<?>) parameterizedType.getActualTypeArguments()[param];
+                        if (paramClazz.isEnum()) {
+                            LOGGER.info(2 + space + paramClazz.getName() + " (enum)");
+                        } else {
+                            String packaage = paramClazz.getPackage().getName();
+                            if (!packaage.startsWith("java")) {
+                                printMembers(space, paramClazz);
+                            } else {
+                                LOGGER.info(3 + space + paramClazz.getName() + " is java lang");
+                            }
+                        }
+                    } else {
+                        LOGGER.info(4 + space + field.getName() + " " + type.getTypeName() + " ??? DONT KNOW WHAT TO DO");
+                    }
+                }
+            } else {
+
+                LOGGER.info(5 + space + field.getName() + " (" + field.getType().getName() + ")");
+
+                if (field.getType().isEnum()) {
+                    LOGGER.info(6 + space + field.getType().getName() + " (enum)");
+                } else {
+                    String packaage = field.getType().getPackage().getName();
+                    if (!packaage.startsWith("java")) {
+                        printMembers(space, field.getType());
+                    } else {
+                        // LOGGER.info(7 + space + field.getName() + " is java lang");
+                    }
+                }
+            }
+        }
+    }
+
 
     private String readFile(String fileName) {
         ClassLoader classLoader = getClass().getClassLoader();

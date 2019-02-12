3436717b89ea173ad9bc62ec6a09594a1247c008
pspex <pspex@adorsys.de>
2019-02-12 20:43:35 +0100

    MUL-326 print class members for mapper

--- onlinebanking-mapper/src/test/java/org/adorsys/multibanking/onlinebanking/mapper/api/MapperTest.java
+++ onlinebanking-mapper/src/test/java/org/adorsys/multibanking/onlinebanking/mapper/api/MapperTest.java
@@ -89,46 +89,60 @@ public class MapperTest {
     }
 
     private void printMembers(String indent, Class<?> clazz) {
-        String space = indent + " " + clazz.getName() + " ";
-        for (Field field : clazz.getDeclaredFields()) {
-            if (("" + field.getType()).contains("interface")) {
-                LOGGER.info(1 + space + field.getName() + " (" + field.getGenericType() + ")");
-                ParameterizedType parameterizedType = (ParameterizedType) field.getGenericType();
-                int numberOfTemplateParams = parameterizedType.getActualTypeArguments().length;
-                for (int param = 0; param < numberOfTemplateParams; param++) {
-                    Type type = parameterizedType.getActualTypeArguments()[param];
-                    if (type instanceof Class) {
-                        Class<?> paramClazz = (Class<?>) parameterizedType.getActualTypeArguments()[param];
-                        if (paramClazz.isEnum()) {
-                            LOGGER.info(2 + space + paramClazz.getName() + " (enum)");
-                        } else {
-                            String packaage = paramClazz.getPackage().getName();
-                            if (!packaage.startsWith("java")) {
-                                printMembers(space, paramClazz);
+        try {
+            String space = indent + " " + clazz.getName() + " ";
+            for (Field field : clazz.getDeclaredFields()) {
+                if (("" + field.getType()).contains("interface")) {
+                    LOGGER.info(1 + space + field.getName() + " (" + field.getGenericType() + ")");
+                    ParameterizedType parameterizedType = (ParameterizedType) field.getGenericType();
+                    int numberOfTemplateParams = parameterizedType.getActualTypeArguments().length;
+                    for (int param = 0; param < numberOfTemplateParams; param++) {
+                        Type type = parameterizedType.getActualTypeArguments()[param];
+                        if (type instanceof Class) {
+                            Class<?> paramClazz = (Class<?>) parameterizedType.getActualTypeArguments()[param];
+                            if (paramClazz.isEnum()) {
+                                LOGGER.info(2 + space + paramClazz.getName() + " (enum)");
                             } else {
-                                LOGGER.info(3 + space + paramClazz.getName() + " is java lang");
+                                String packaage = paramClazz.getPackage().getName();
+                                if (!packaage.startsWith("java")) {
+                                    printMembers(space, paramClazz);
+                                } else {
+                                    LOGGER.info(3 + space + paramClazz.getName() + " is java lang");
+                                }
+                            }
+                        } else {
+                            if (("" + type).contains("interface")) {
+                                LOGGER.info(4.5 + space + field.getName() + " " + type.getTypeName() + " could continue with interface type");
+                            } else {
+                                LOGGER.info(4.6 + space + field.getName() + " " + type.getTypeName() + " ??? DONT KNOW WHAT TO DO");
+                                LOGGER.info(4.7 + space + field.getName() + " " + type + " ??? DONT KNOW WHAT TO DO");
+                                LOGGER.info(4.8 + space + field.getName() + " " + type.getClass() + " ??? DONT KNOW WHAT TO DO");
+                                // Class<?> clazzy = Class.forName(type.getTypeName());
+                              //  Class<?> clazzy = type.getClass();
+                              //   printMembers(space, clazzy);
                             }
                         }
-                    } else {
-                        LOGGER.info(4 + space + field.getName() + " " + type.getTypeName() + " ??? DONT KNOW WHAT TO DO");
                     }
-                }
-            } else {
-
-                LOGGER.info(5 + space + field.getName() + " (" + field.getType().getName() + ")");
-
-                if (field.getType().isEnum()) {
-                    LOGGER.info(6 + space + field.getType().getName() + " (enum)");
                 } else {
-                    String packaage = field.getType().getPackage().getName();
-                    if (!packaage.startsWith("java")) {
-                        printMembers(space, field.getType());
+
+                    LOGGER.info(5 + space + field.getName() + " (" + field.getType().getName() + ")");
+
+                    if (field.getType().isEnum()) {
+                        LOGGER.info(6 + space + field.getType().getName() + " (enum)");
                     } else {
-                        // LOGGER.info(7 + space + field.getName() + " is java lang");
+                        String packaage = field.getType().getPackage().getName();
+                        if (!packaage.startsWith("java")) {
+                            printMembers(space, field.getType());
+                        } else {
+                            // LOGGER.info(7 + space + field.getName() + " is java lang");
+                        }
                     }
                 }
             }
+        } catch (Exception e) {
+            throw BaseExceptionHandler.handle(e);
         }
+
     }
 
 
